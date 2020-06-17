bbe4ee5db7531a94a8b9351c79910db92de8729d
mo71.mohammadi <mo71.mohammadi@gmail.com>
2020-06-17 15:13:38 +0430

    cpanel commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-require("dotenv").config();
-const express = require('express');
+// require("dotenv").config();
+// const express = require('express');
 const mongoose = require('mongoose');
-const app = express();
+// const app = express();
 const genericsRouter = require('./routes/generics');
 const recommendsRouter = require('./routes/recommends');
 const insurancesRouter = require('./routes/insurances');
