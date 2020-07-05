9681bc968b91eadadc7be349c7c31b75dfc59ed6
mo71.mohammadi <mo71.mohammadi@gmail.com>
2020-07-05 17:31:17 +0430

    import MongoDB

--- routes/import.js
+++ routes/import.js
@@ -9,24 +9,26 @@ const query = require('url');
 
 exports.import = async (req, res) => {
 	try {
-		const params = query.parse(req.url, true).query;
-		fs.readFile(`./temp/${params['file']}.json`,"utf8",async (err, data) => {
-			if (err) throw err;
-			let newData = data.replace(/"_id":{.*?},/g, '')
-			let student = JSON.parse(newData)
-			for (const record of student) {
-				delete record._id
-			}
-			let colObj = {
-				Recommend: Recommend,
-				MedScape: MedScape,
-				UpToDate: UpToDate,
-				Drug: Drug
-			}
-			colObj[params['collection']].insertMany(student).then(result => {
-				res.status(200).json(result.length)
-			})
-		});
+				res.status(200).json("result.length")
+
+		// const params = query.parse(req.url, true).query;
+		// fs.readFile(`./temp/${params['file']}.json`,"utf8",async (err, data) => {
+		// 	if (err) throw err;
+		// 	let newData = data.replace(/"_id":{.*?},/g, '')
+		// 	let student = JSON.parse(newData)
+		// 	for (const record of student) {
+		// 		delete record._id
+		// 	}
+		// 	let colObj = {
+		// 		Recommend: Recommend,
+		// 		MedScape: MedScape,
+		// 		UpToDate: UpToDate,
+		// 		Drug: Drug
+		// 	}
+		// 	colObj[params['collection']].insertMany(student).then(result => {
+		// 		res.status(200).json(result.length)
+		// 	})
+		// });
 	} catch (e) {
 		res.status(200).json({message: e.message})
 	}
